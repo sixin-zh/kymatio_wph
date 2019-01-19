1b95902c66bb81994573d4bd88df776c106049f9
Sixin <me at home>
2019-01-19 08:40:09 +0100

    add init version etc for kymatio

---
+++ kymatio/__init__.py
@@ -0,0 +1,21 @@
+# Make sure that DeprecationWarning within this package always gets printed
+### Snippet copied from sklearn.__init__
+import warnings
+import re
+warnings.filterwarnings('always', category=DeprecationWarning,
+                        module=r'^{0}.*'.format(re.escape(__name__)))
+### End Snippet
+
+__all__ = [
+            'Scattering1D',
+            'Scattering2D',
+            'Scattering3D',
+#	    'Scat2o1',
+            ]
+
+from .scattering1d.scattering1d import Scattering1D
+from .scattering2d.scattering2d import Scattering2D
+from .scattering3d.scattering3d import Scattering3D
+#from .scatrec2d.scattering2d_pp import Scat2o1
+
+from .version import version as __version__
