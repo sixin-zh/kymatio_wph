f2058bbfe68d0e3f1d44c00a1bccc69e0860353a
Sixin <me at home>
2019-01-21 18:46:14 +0100

    sync from sixin

--- kymatio/phaseharmonics2d/tests/test_idx_dirac2d.py
+++ kymatio/phaseharmonics2d/tests/test_idx_dirac2d.py
@@ -39,11 +39,11 @@ wph_op = PhaseHarmonics2d(M, N, J, L, j_max, l_max)
 Sim = wph_op(im) # (nb,nc,nb_channels,1,1,2)
 nbc = Sim.shape[2]
 for idxbc in range(nbc):
-    j1 = wph_op.idx_wph['la1']//L
-    theta1 = wph_op.idx_wph['la1']%L
+    j1 = wph_op.idx_wph['la1'][idxbc]//L
+    theta1 = wph_op.idx_wph['la1'][idxbc]%L
     k1 = wph_op.idx_wph['k1'][0,idxbc,0,0]
-    j2 = wph_op.idx_wph['la2']//L
-    theta2 = wph_op.idx_wph['la2']%L
+    j2 = wph_op.idx_wph['la2'][idxbc]//L
+    theta2 = wph_op.idx_wph['la2'][idxbc]%L
     k2 = wph_op.idx_wph['k2'][0,idxbc,0,0]
     val = (j1,theta1,k1,j2,theta2,k2)
     print(idxbc, "=>" , val,  Sim[0,0,idxbc,0,0,0],  "+i ",Sim[0,0,idxbc,0,0,1] )
