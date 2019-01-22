d68d32fce38efa4e6315e2c4924f57f470d3f196
Sixin <me at home>
2019-01-22 17:07:28 +0100

    sync from sixin

--- kymatio/phaseharmonics2d/phase_harmonics_k_bump.py
+++ kymatio/phaseharmonics2d/phase_harmonics_k_bump.py
@@ -167,7 +167,7 @@ class PhaseHarmonics2d(object):
         """
         print('call cuda')
         self.idx_wph['la1'] = self.idx_wph['la1'].type(torch.cuda.LongTensor)
-        self.idx_wph['la2'] = self.idx_wph['la1'].type(torch.cuda.LongTensor)
+        self.idx_wph['la2'] = self.idx_wph['la2'].type(torch.cuda.LongTensor)
         self.idx_wph['k1'] = self.idx_wph['k1'].type(torch.cuda.FloatTensor)
         self.idx_wph['k2'] = self.idx_wph['k2'].type(torch.cuda.FloatTensor)
         
