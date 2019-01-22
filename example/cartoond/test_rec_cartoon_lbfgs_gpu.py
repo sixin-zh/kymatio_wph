b60e77537435a954744ed61224669b524dbcac08
Sixin <me at home>
2019-01-22 20:55:26 +0100

    sync from sixin

--- example/cartoond/test_rec_cartoon_lbfgs_gpu.py
+++ example/cartoond/test_rec_cartoon_lbfgs_gpu.py
@@ -19,11 +19,8 @@ size=64
 # --- Dirac example---#
 
 data = sio.loadmat('./example/cartoond/demo_toy7d_N64.mat')
-print(data['imgs'].shape)
-im = np.zeros((size,size))
-im[15,15] = 1
+im = data['imgs']
 im = torch.tensor(im, dtype=torch.float).unsqueeze(0).unsqueeze(0).cuda()
-
 
 # Parameters for transforms
 
