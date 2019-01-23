8f96bd645e34525c213478e89dd30ab7283e41e6
Sixin <me at home>
2019-01-23 12:38:32 +0100

    sync from sixin

--- kymatio/phaseharmonics2d/phase_harmonics_k_bump_chunks.py
+++ kymatio/phaseharmonics2d/phase_harmonics_k_bump_chunks.py
@@ -93,7 +93,7 @@ class PhaseHarmonics2d(object):
         print('nb cov is', nb_cov)
         
         n_chunks = math.ceil(nb_cov/self.max_chunk)
-        nb_cov_chunk = np.zeros(n_chunks)
+        nb_cov_chunk = np.zeros(n_chunks,dtype=np.int32)
         for idxc in range(n_chunks):
             if idxc < n_chunks-1:
                 nb_cov_chunk[idxc] = int(self.max_chunk)
