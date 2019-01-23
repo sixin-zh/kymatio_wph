9d474c8daf95d810d66e42cb2d6223dc2e9ac137
sixzhang <sixin.zhang@ens.fr>
2019-01-23 16:28:53 +0100

    test

--- kymatio/phaseharmonics2d/tests/test_bump_dirac2d.py
+++ kymatio/phaseharmonics2d/tests/test_bump_dirac2d.py
@@ -39,22 +39,22 @@ wph_op = PhaseHarmonics2d(M, N, J, L, delta_j, delta_l, delta_k)
 
 L2 = L*2
 Sim = wph_op(im) # (nb,nc,nb_channels,1,1,2)
-#print(Sim.sum())
+print('sum', Sim.sum())
 
-nbc = Sim.shape[2]
-for idxbc in range(nbc-1):
-    j1 = wph_op.idx_wph['la1'][idxbc]//L2
-    theta1 = wph_op.idx_wph['la1'][idxbc]%L2
-    k1 = wph_op.idx_wph['k1'][0,idxbc,0,0]
-    j2 = wph_op.idx_wph['la2'][idxbc]//L2
-    theta2 = wph_op.idx_wph['la2'][idxbc]%L2
-    k2 = wph_op.idx_wph['k2'][0,idxbc,0,0]
-    val = (int(j1),int(theta1),int(k1),int(j2),int(theta2),int(k2))
-    if idxbc == 384: 
-        print(idxbc, "=>" , val,  float(Sim[0,0,idxbc,0,0,0]),  "+i ",float(Sim[0,0,idxbc,0,0,1]) )
+#nbc = Sim.shape[2]
+#for idxbc in range(nbc-1):
+#    j1 = wph_op.idx_wph['la1'][idxbc]//L2
+#    theta1 = wph_op.idx_wph['la1'][idxbc]%L2
+#    k1 = wph_op.idx_wph['k1'][0,idxbc,0,0]
+#    j2 = wph_op.idx_wph['la2'][idxbc]//L2
+#    theta2 = wph_op.idx_wph['la2'][idxbc]%L2
+#    k2 = wph_op.idx_wph['k2'][0,idxbc,0,0]
+#    val = (int(j1),int(theta1),int(k1),int(j2),int(theta2),int(k2))
+#    if idxbc == 384: 
+#        print(idxbc, "=>" , val,  float(Sim[0,0,idxbc,0,0,0]),  "+i ",float(Sim[0,0,idxbc,0,0,1]) )
 
 # last channel is l2
 #print(nbc-1, "=> phiJ " ,  float(Sim[0,0,nbc-1,0,0,0]),  "+i ",float(Sim[0,0,nbc-1,0,0,1]) )
 
-plt.plot(Sim.squeeze().numpy()[...,0])
-plt.show()
+#plt.plot(Sim.squeeze().numpy()[...,0])
+#plt.show()
