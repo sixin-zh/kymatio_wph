7b7f0fa43f72018b34098d8a7ee3f9cf790010b4
Sixin <me at home>
2019-01-24 10:58:47 +0100

    sync from sixin

--- example/cartoond/test_rec_bump_par_cartoon_lbfgs_gpu_N128.py
+++ example/cartoond/test_rec_bump_par_cartoon_lbfgs_gpu_N128.py
@@ -95,7 +95,7 @@ def callback_print(x):
         print(loss)
 
         for obj in gc.get_objects():
-            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+            if torch.is_tensor(obj): #  or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
                 print(type(obj), obj.size())
     
         return loss
