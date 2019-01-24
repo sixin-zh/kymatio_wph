c651ff6cd57b30a0ea8ae10b3df00f41024353e3
Sixin <me at home>
2019-01-24 14:13:36 +0100

    sync from sixin

--- kymatio/phaseharmonics2d/tests/test_bump_dirac2d_chunkid.py
+++ kymatio/phaseharmonics2d/tests/test_bump_dirac2d_chunkid.py
@@ -36,10 +36,9 @@ from kymatio.phaseharmonics2d.phase_harmonics_k_bump_chunkid \
     import PhaseHarmonics2d
 
 nb_chunks = 10
-chunk_id = 10
 Sims = []
 #n_coeff = 0
-for chunk_in in range(nb_chunks):
+for chunk_id in range(nb_chunks):
     wph_op = PhaseHarmonics2d(M, N, J, L, delta_j, delta_l, delta_k, nb_chunks, chunk_id)
     Sim_ = wph_op(im) # (nb,nc,nb_channels,1,1,2)
     print('Sim_',Sim_.shape)
