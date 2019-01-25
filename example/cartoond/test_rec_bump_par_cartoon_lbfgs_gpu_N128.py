d881d8d88155b9b43cd60fa08b1a87e0f54f8f0c
Sixin <me at home>
2019-01-25 15:43:19 +0100

    sync from sixin

--- example/cartoond/test_rec_bump_par_cartoon_lbfgs_gpu_N128.py
+++ example/cartoond/test_rec_bump_par_cartoon_lbfgs_gpu_N128.py
@@ -105,7 +105,7 @@ def callback_print(x):
         
         return loss
 
-x = torch.Tensor(1, 1, N, N).normal_(std=0.1)
+x = torch.Tensor(1, 1, N, N).normal_(std=0.01) + 0.5
 #x[0,0,0,0] = 2
 #x = torch.tensor(x, requires_grad=True)
 x0 = x.reshape(size**2).detach().numpy()
