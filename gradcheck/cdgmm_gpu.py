e9133da2d47dda47e28b659435110024a2f9a71e
Sixin <me at home>
2019-01-29 12:59:53 +0100

    sync from sixin

---
+++ gradcheck/cdgmm_gpu.py
@@ -0,0 +1,22 @@
+
+import torch
+from torch.autograd import gradcheck
+
+nb=1
+nc=1
+M=2
+N=2
+A = torch.zeros((nb,nc,M,N,2),dtype=torch.float32).cuda()
+B = torch.zeros((M,N,2),dtype=torch.float32).cuda()
+
+from kymatio.phaseharmonics2d.backend import cdgmm
+
+#C = cdgmm(A,B)
+A.requires_grad_(True)
+B.requires_grad_(True)
+result = gradcheck(cdgmm, (A,B))
+if result:
+    print('pass')
+else:
+    print('not pass')
+    
