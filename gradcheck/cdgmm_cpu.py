7eb83c712387455a7ea5297efc7db519c3ff67f6
Sixin <me at home>
2019-01-29 12:50:10 +0100

    sync from sixin

--- gradcheck/cdgmm_cpu.py
+++ gradcheck/cdgmm_cpu.py
@@ -7,7 +7,7 @@ nc=1
 M=2
 N=2
 A = torch.zeros((nb,nc,M,N,2),dtype=torch.float32)
-B = torch.FloatTensor((M,N,2),dtype=torch.float32)
+B = torch.zeros((M,N,2),dtype=torch.float32)
 print(A.shape,B.shape)
 
 from kymatio.phaseharmonics2d.backend import cdgmm
