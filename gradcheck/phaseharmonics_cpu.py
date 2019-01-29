4b201dee16f5610aa453756b6ff009c8158beb97
Sixin <me at home>
2019-01-29 13:35:21 +0100

    sync from sixin

--- gradcheck/phaseharmonics_cpu.py
+++ gradcheck/phaseharmonics_cpu.py
@@ -5,8 +5,8 @@ from torch.autograd import gradcheck
 Pc=2
 M=2
 N=2
-A = torch.zeros((1,Pc,M,N,2),dtype=torch.float32)
-k = torch.zeros((1,Pc,1,1),dtype=torch.float32)
+A = torch.randn((1,Pc,M,N,2),dtype=torch.float32)
+k = torch.ones((1,Pc,1,1),dtype=torch.float32)*2
 
 from kymatio.phaseharmonics2d.backend import PhaseHarmonic
 phase_harmonics = PhaseHarmonic(check_for_nan=False)
