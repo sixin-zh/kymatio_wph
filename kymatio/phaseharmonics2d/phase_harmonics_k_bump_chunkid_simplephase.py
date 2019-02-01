f4e8078b239f62dffe990426d6ae45face92ea81
Sixin <me at home>
2019-02-02 07:04:13 +0800

    sync from sixin

--- kymatio/phaseharmonics2d/phase_harmonics_k_bump_chunkid_simplephase.py
+++ kymatio/phaseharmonics2d/phase_harmonics_k_bump_chunkid_simplephase.py
@@ -198,9 +198,9 @@ class PhaseHarmonics2d(object):
                                     hit_nb1[(j2,k2,ell2)]=2
                                     hit_nb2[(j1,k1,ell1,j2,k2,ell2)]=2
 
-        print('hit nb1 values',hit_nb1.values())
-        nb1 = np.array(hit_nb1.values(), dtype=int).sum() + 1 # plus last phiJ channel
-        nb2 = np.array(hit_nb2.values(), dtype=int).sum() + 1 # complex-valued, plus last phiJ channel
+        print('hit nb1 values',list(hit_nb1.values()))
+        nb1 = np.array(list(hit_nb1.values()), dtype=int).sum() + 1 # plus last phiJ channel
+        nb2 = np.array(list(hit_nb2.values()), dtype=int).sum() + 1 # complex-valued, plus last phiJ channel
 
         return nb1, nb2
     
