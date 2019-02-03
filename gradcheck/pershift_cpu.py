7dfa4842cea00af516a27f8dd665404d851458a8
Sixin <me at home>
2019-02-03 22:28:14 +0800

    sync from sixin

--- gradcheck/pershift_cpu.py
+++ gradcheck/pershift_cpu.py
@@ -13,7 +13,7 @@ shift2 = 1
 from kymatio.phaseharmonics2d.backend import PeriodicShift2D
 pershift = PeriodicShift2D.apply
 Y = pershift(X,shift1,shift2)
-print(X,Y)
+print(X[0,0,:,:,0],Y[0,0,:,:,0])
 
 X.requires_grad_(True)
 result = gradcheck(pershift, (X,shift1,shift2), eps=1e-4, atol=1e-2, rtol=1e-3)
