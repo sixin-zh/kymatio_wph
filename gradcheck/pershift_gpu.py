06f03996c148064334f29088d6d76e09c195ccf7
Sixin <me at home>
2019-02-04 12:50:34 +0800

    sync from sixin

---
+++ gradcheck/pershift_gpu.py
@@ -0,0 +1,25 @@
+
+import torch
+from torch.autograd import gradcheck
+
+nb=1
+nc=1
+M=4
+N=4
+X = torch.randn((nb,nc,M,N,2),dtype=torch.float32).cuda()
+shift1 = 1
+shift2 = 1
+
+from kymatio.phaseharmonics2d.backend import PeriodicShift2D
+pershift = PeriodicShift2D(M,N,shift1,shift2)
+
+Y = pershift(X)
+print(X[0,0,:,:,0],Y[0,0,:,:,0])
+
+X.requires_grad_(True)
+result = gradcheck(pershift, X, eps=1e-4, atol=1e-2, rtol=1e-3)
+if result:
+    print('pass')
+else:
+    print('not pass')
+    
