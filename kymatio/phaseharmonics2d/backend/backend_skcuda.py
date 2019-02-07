2847b963443843d5d9352d84397c65f83c302dde
Sixin <me at home>
2019-02-08 04:11:08 +0800

    sync from sixin

--- kymatio/phaseharmonics2d/backend/backend_skcuda.py
+++ kymatio/phaseharmonics2d/backend/backend_skcuda.py
@@ -156,6 +156,8 @@ class cdgmmMulcu(Function):
         A, B = ctx.saved_tensors
         conjA = A.clone()
         conjB = B.clone()
+        conjA[:,:,:,:,1] = -A[:,:,:,:,1]
+        conjB[:,:,1] = -B[:,:,1]
         m, n = conjB.nelement() // 2, conjA.nelement() // conjB.nelement()
         # n is the B*C
         # m is the M*N
