d35a691d70e5afbb90b4667009c0ff47572a68cd
Sixin <me at home>
2019-02-07 09:42:40 +0800

    sync from sixin

--- gradcheck/pershift_cpu.py
+++ gradcheck/pershift_cpu.py
@@ -7,14 +7,15 @@ nc=1
 M=4
 N=4
 X = torch.randn((nb,nc,M,N,2),dtype=torch.float32)
-shift1 = 1
-shift2 = 1
+shift1 = -1
+shift2 = -1
 
 from kymatio.phaseharmonics2d.backend import PeriodicShift2D
 pershift = PeriodicShift2D(M,N,shift1,shift2)
 
 Y = pershift(X)
-print(X[0,0,:,:,0],Y[0,0,:,:,0])
+print(X[0,0,:,:,0])
+print(Y[0,0,:,:,0])
 
 X.requires_grad_(True)
 result = gradcheck(pershift, X, eps=1e-4, atol=1e-2, rtol=1e-3)
