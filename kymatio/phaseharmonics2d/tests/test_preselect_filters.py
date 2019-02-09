988013dd44b7f6029e62529b1ca1d85665c6333a
Sixin <me at home>
2019-02-09 08:04:17 +0800

    to add preselect filters to speedup the computation

---
+++ kymatio/phaseharmonics2d/tests/test_preselect_filters.py
@@ -0,0 +1,41 @@
+
+import scipy.optimize as opt
+import scipy.io as sio
+
+import torch
+from torch.autograd import Variable, grad
+
+from time import time
+import gc
+
+size=256
+
+# --- Dirac example---#
+data = sio.loadmat('./data/demo_toy7d_N' + str(size) + '.mat')
+im = data['imgs']
+im = torch.tensor(im, dtype=torch.float).unsqueeze(0).unsqueeze(0).cuda()
+
+# Parameters for transforms
+J = 8
+L = 8
+M, N = im.shape[-2], im.shape[-1]
+delta_j = 1
+delta_l = L/2
+delta_k = 1
+delta_n = 1
+nb_chunks = 10
+nb_restarts = 10
+nGPU = 1
+factr = 1e5
+
+from kymatio.phaseharmonics2d.phase_harmonics_k_bump_chunkid_pershift \
+    import PHkPerShift2d
+
+dn1 = 0
+dn2 = 0
+chunk_id = 0
+devid = 0
+wph_op = PHkPerShift2d(M, N, J, L, dn1, dn2, delta_l, J, chunk_id, devid)
+Sim = wph_op(im)
+
+print(Sim)
