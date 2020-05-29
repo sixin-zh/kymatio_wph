aca12f14a30428e5b18f3794825cefdc9e225a5f
sixzhang <sixin.zhang@ens.fr>
2020-05-29 23:17:30 +0200

    the pp lbfgs2 par scales well: cleanup

--- pp/lbfgs2_routine_par.py
+++ pp/lbfgs2_routine_par.py
@@ -25,49 +25,6 @@ def obj_func_id(x_t,wph_ops,wph_streams,Sims,factr2,sigma,ress,Mxs,Mys,pis,op_id
         
     return loss
 
-'''    
-def obj_func_id_par2(x,wph_ops,wph_streams,Sims,factr2,sigma,ress,Mxs,Mys,pis,op_id,nGPU):
-    # cut points to nGPU, then compute each pieced images
-    xlen = x.shape[0]
-    avglen = int(xlen/nGPU)
-    offset = 0
-    im_a = []
-    for devid in range(nGPU):
-        res_t = ress[devid]
-        Mx_t = Mxs[devid]
-        My_t = Mys[devid]
-        pi_t = pis[devid]
-        if devid < nGPU-1:
-            x_t = x.narrow(0,offset,avglen).to(devid)
-        else:
-            x_t = x.narrow(0,offset,xlen-offset).to(devid)
-        with torch.cuda.device(devid):
-            torch.cuda.stream(wph_streams[devid])
-            im_t = pos_to_im3(x_t, res_t, Mx_t, My_t, pi_t, sigma)
-            im_a.append(im_t)
-        offset += avglen
-    #torch.cuda.synchronize()
-    
-    # sum to an im on gpu 0
-    for devid in range(nGPU):        
-        if devid == 0:
-            im = im_a[devid].to(0)
-        else:
-            im += im_a[devid].to(0)
-
-    # compute wph grad on devid
-    devid = op_id % nGPU
-    im_ = im.to(devid)
-    wph_op = wph_ops[op_id]
-    with torch.cuda.device(devid):
-        torch.cuda.stream(wph_streams[devid])
-        p = wph_op(im_)
-        diff = p-Sims[op_id]
-        loss = torch.mul(diff,diff).sum()
-        loss = loss*factr2
-    
-    return loss
-'''
 def obj_func(x,wph_ops,wph_streams,Sims,factr2,sigma,ress,Mxs,Mys,pis,nGPU):
     loss = 0
     loss_a = []
